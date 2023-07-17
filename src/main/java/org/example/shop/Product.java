72db96356e9cb81dd80b137cae08aacb8b29aef0
Paulina Rogiska <paula316@wp.pl>
2023-07-17 19:06:32 +0200

    first model record Product2

--- src/main/java/org/example/shop/Product.java
+++ src/main/java/org/example/shop/Product.java
@@ -1,6 +1,8 @@
 package org.example.shop;
 
-public record Product(int productId, double price, String name/*,Category category*/, int quantity) {
+import org.example.Category;
+
+public record Product(int productId, double price, String name, Category category, int quantity) {
 
 
 }
