7c9080eefd7f69477d9bf12dabab80b89ba5ed99
Paulina Rogiska <paula316@wp.pl>
2023-07-24 21:23:04 +0200

    another menu changes

--- src/main/java/shop/Menu.java
+++ src/main/java/shop/Menu.java
@@ -13,7 +13,6 @@ public class Menu {
     private final ProductService productService = new ProductService(categoryService);
     private final OrderService orderService = new OrderService(productService);
 
-
     private void generateCategories() {
         categoryService.addCategory("Sprzęt AGD");
         categoryService.addCategory("Sprzęt RTV");
@@ -22,40 +21,27 @@ public class Menu {
         categoryService.addCategory("Ubrania");
     }
 
-    private List<Product> generateProducts() {
-        List<Product> productList = new ArrayList<>();
-
-        Product product1 = new Product(1, 1000, "Pralka",
+    private void generateProducts() {
+        productService.addProduct(1000, "Pralka",
                 categoryService.getCategories().get(0));
-        Product product2 = new Product(2, 3000, "Lodówka",
+        productService.addProduct(2500, "Lodówka",
                 categoryService.getCategories().get(0));
-        Product product3 = new Product(3, 5000, "Laptop",
+        productService.addProduct(500, "Laptop",
                 categoryService.getCategories().get(1));
-        Product product4 = new Product(4, 4500.50, "Smartfon",
+        productService.addProduct(4500.50, "Smartfon",
                 categoryService.getCategories().get(1));
-        Product product5 = new Product(5, 150, "Barbie",
+        productService.addProduct(150, "Barbie",
                 categoryService.getCategories().get(2));
-        Product product6 = new Product(6, 50, "Puzzle",
+        productService.addProduct(50, "Puzzle",
                 categoryService.getCategories().get(2));
-        Product product7 = new Product(7, 900.50, "Stół",
+        productService.addProduct(900.50, "Stół",
                 categoryService.getCategories().get(3));
-        Product product8 = new Product(8, 4000, "Sofa",
+        productService.addProduct(4000, "Sofa",
                 categoryService.getCategories().get(3));
-        Product product9 = new Product(9, 250, "Sukienka",
+        productService.addProduct(250, "Sukienka",
                 categoryService.getCategories().get(4));
-        Product product10 = new Product(10, 300, "Kurtka",
+        productService.addProduct(300, "Kurtka",
                 categoryService.getCategories().get(4));
-        productList.add(product1);
-        productList.add(product2);
-        productList.add(product3);
-        productList.add(product4);
-        productList.add(product5);
-        productList.add(product6);
-        productList.add(product7);
-        productList.add(product8);
-        productList.add(product9);
-        productList.add(product10);
-        return productList;
     }
 
     private List<Order> generateOrders() {
